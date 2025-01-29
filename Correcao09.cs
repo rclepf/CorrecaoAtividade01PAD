8213d826c3763ba5f4ad4a38ebbb46ac4f409ea5
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 10:30:08 -0300

    Update Correcao09.cs

--- Correcao09.cs
+++ Correcao09.cs
@@ -2,11 +2,11 @@ class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.Write("Digite uma palavra: ");
+		Console.Write("Digite uma palavra: ");
 	        string palavra = Console.ReadLine();
 	        Console.WriteLine("O comprimento da palavra " + palavra +" é: " + palavra.Length);
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+		Console.Write("Press any key to continue . . . ");
+		Console.ReadKey(true);
 		}
 	}
