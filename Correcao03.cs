2415203a35d9ca46185a3b74cf6c9ee5c70c428e
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 09:23:51 -0300

    Create Correcao03.cs

---
+++ Correcao03.cs
@@ -0,0 +1,13 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite um número de ponto flutuante: ");
+	        float numeroFloat = float.Parse(Console.ReadLine());
+	        Console.WriteLine("Você digitou: " + numeroFloat);
+
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
