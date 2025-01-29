36f552d59235ba25a766e4caf9c9849b3cc50a88
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 09:27:13 -0300

    Create Correcao10.cs

---
+++ Correcao10.cs
@@ -0,0 +1,22 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite o número da casa: ");
+	        string numeroCasa = Console.ReadLine();
+	        Console.Write("Digite a rua: ");
+	        string rua = Console.ReadLine();
+	        Console.Write("Digite o bairro: ");
+	        string bairro = Console.ReadLine();
+	        Console.Write("Digite a cidade: ");
+	        string cidade = Console.ReadLine();
+	        Console.Write("Digite o estado: ");
+	        string estado = Console.ReadLine();
+	        Console.WriteLine("Endereço: {0}, {1}, {2}, {3}/{4}", rua, numeroCasa, bairro, cidade, estado);
+	        
+        	Console.WriteLine("Endereço: " + rua + ", " + numeroCasa+ ", "+bairro+", "+cidade+"/"+estado);
+				
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
