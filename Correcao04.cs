e9ab924e5443810666148689d21addba1f3312e2
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 09:24:13 -0300

    Create Correcao04.cs

---
+++ Correcao04.cs
@@ -0,0 +1,12 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite 'sim' ou 'não': ");
+	        bool respostaBool = Console.ReadLine().ToLower() == "sim";
+	        Console.WriteLine("Você digitou: " + respostaBool);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
