f0fe0b7150a3bd4f172b27d543bc6a58e76a125b
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 09:25:39 -0300

    Create Correcao06.cs

---
+++ Correcao06.cs
@@ -0,0 +1,19 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite um número decimal: ");
+	        decimal numeroDecimal = decimal.Parse(Console.ReadLine());
+	        Console.WriteLine("Você digitou: " + numeroDecimal);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			
+			//O tipo decimal no C# é um tipo de dado numérico de alta precisão, utilizado principalmente para cálculos financeiros e monetários, onde a precisão é essencial para evitar erros de arredondamento.
+			
+			//Tipo	Tamanho (bits)	Precisão		Indicado para
+			//float		32			~7 dígitos		Gráficos, jogos, simulações
+			//double		64		~15-16 dígitos	Cálculos científicos, matemática complexa
+			//decimal		128		~28-29 dígitos	Cálculos financeiros, contabilidade
+		}
+	}
