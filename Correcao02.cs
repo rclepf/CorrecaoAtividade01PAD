22677957c19f42889d2c2fca08757a7e8c532d1a
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 10:32:22 -0300

    Update Correcao02.cs

--- Correcao02.cs
+++ Correcao02.cs
@@ -2,11 +2,11 @@ class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.Write("Digite um número real: ");
+		Console.Write("Digite um número real: ");
 	        double numeroReal = double.Parse(Console.ReadLine());
 	        Console.WriteLine("Você digitou: " + numeroReal);
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+		Console.Write("Press any key to continue . . . ");
+		Console.ReadKey(true);
 		}
 	}
