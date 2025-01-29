23781f25b6b6bc618cf581ff1d001e7ea2aea6f2
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 10:30:25 -0300

    Update Correcao08.cs

--- Correcao08.cs
+++ Correcao08.cs
@@ -2,13 +2,13 @@ class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.Write("Digite o preço do produto: ");
+		Console.Write("Digite o preço do produto: ");
 	        double preco = double.Parse(Console.ReadLine());
 	        Console.Write("Digite o desconto (em %): ");
 	        double desconto = double.Parse(Console.ReadLine());
 	        double precoFinal = preco - (preco * desconto / 100);
         	Console.WriteLine("Preço final com desconto: " + precoFinal);
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+		Console.Write("Press any key to continue . . . ");
+		Console.ReadKey(true);
 		}
 	}
