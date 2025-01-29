ccfeab450fcf2b0269c5f5ea01ddb2eec7393b12
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-01-29 09:24:55 -0300

    Create Correcao05.cs

---
+++ Correcao05.cs
@@ -0,0 +1,12 @@
+class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite um caractere: ");
+	        char caractere = Console.ReadKey().KeyChar;
+	        Console.WriteLine("\nVocê digitou: " + caractere);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
